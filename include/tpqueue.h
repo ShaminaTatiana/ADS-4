40a162f7598b4cf8a01616865f9e2859cbe46889
ShaminaTatiana <78913652+ShaminaTatiana@users.noreply.github.com>
2021-06-01 16:07:09 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,7 +2,6 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 #include <cassert>
-
 template<typename T>
 class TPQueue {
   // Сюда помещается описание структуры "Очередь с приоритетами"
@@ -67,4 +66,5 @@ struct SYM {
   char ch;
   int  prior;
 };
+
 #endif // INCLUDE_TPQUEUE_H_
